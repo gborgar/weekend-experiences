cddbe9a6a9931aed78d1d2bca1b76b69cd3d1e63
Gustavo Borrero <gustavo.borrero.garcia@gmail.com>
2022-01-30 17:28:43 +0100

    feat: detail controller and fix: social media icons links

--- controllers/reservations.controller.js
+++ controllers/reservations.controller.js
@@ -11,30 +11,29 @@ module.exports.doTravelFind = (req, res, next) => {
         .then(travels => {
             res.render("reservations/travel-finder", {travels, finder, countries, documentTypes})
         })
-        .catch(error => next(error))
+        .catch(error => next(error));
 };
 
 module.exports.doTravelBook = (req, res, next) => {
     const booking = req.body;
-    console.log(booking);
     booking.owner = req.user.id;
-    Travel.find({destination:booking.destination})
-        .then(travels => {
-            const validTravels = travels.filter(travel => !booking.travelChoices.includes(travel.id));
-            booking.travel = validTravels[Math.floor(Math.random() * validTravels.length)];
-            console.log("booking");
-           console.log(booking);  
-           Booking.create(booking)
-            .then(booking => res.redirect(`/bookings/${booking.id}`))
-            .catch(error => next(error))
-        })
+    console.log(booking);
+    booking.travel = booking.travelChoices[Math.floor(Math.random() * booking.travelChoices.length)];
+    console.log(booking);
+        Booking.create(booking)
+        .then(booking => res.redirect(`/bookings/${booking.id}`))
         .catch(error => next(error));
 }
 
-
 module.exports.detail = (req, res, next) => {
-    // buscar el booking en base de datos por id (req.params.id)
-    // Booking.findById(req.params.id).populate("travel").populate("owner")
-    // render("reservations/detail", { booking })
-    res.render("reservations/detail")
+    Booking.findById(req.params.id).populate("travel").populate("owner")
+    .then(booking => {
+        if (booking) {
+            return Travel.findById(booking.travel).then((travel) =>
+                res.render("reservations/detail", { booking, travel }));
+        } else {
+            next(createError(404, "Travel not found"));
+        }
+    })
+    .catch(error => next(error));
 }
